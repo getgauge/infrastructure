173435790ae9016f2021918c39285a3794fec9b7
kashishm <kashishmunjal64@gmail.com>
2017-05-30 11:49:39 +0530

    Updating Vagrantfile content

--- osx/gvm.go
+++ osx/gvm.go
@@ -16,7 +16,8 @@ import (
 var vagrantFile = `
 Vagrant.configure("2") do |config|
   config.vm.box = "%s"
-  config.vm.network :private_network, ip: "192.168.0.0"
+  config.vm.network :private_network, ip: "192.168.33.10"
+  config.vm.synced_folder ".", "/vagrant", type: "nfs"
 end
 `
 
