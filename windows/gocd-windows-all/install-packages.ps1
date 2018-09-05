b55a627ed93dd24ce6b869615d2df9af5cc7389a
Shubham Singh <s.chauhan5505@gmail.com>
2018-09-05 10:43:42 +0530

    Added language dependency to install

--- windows/gocd-windows-all/install-packages.ps1
+++ windows/gocd-windows-all/install-packages.ps1
@@ -11,13 +11,12 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object
 
 # install packages
 cinst git -y
-# cinst golang -y
-# cinst jdk8 -y
-# cinst maven -y
-# cinst ruby -version 2.4 -y
+cinst golang -y
+cinst jdk8 -y
+cinst maven -y
 # cinst dotnet4.5 -y
 # cinst nvm -y
-# cinst nodejs.install --version 9.4.0 -y
+cinst nodejs.install --version 9.4.0 -y
 # cinst dotnetcore-sdk -y
 # cinst python3 -y
 # cinst pip -y
